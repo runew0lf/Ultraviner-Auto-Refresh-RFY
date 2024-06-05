0cbc50fb6509ef946eddcc814c583a4f75b93b6b
Runew0lf <runew0lf@gmail.com>
2024-06-05 22:40:13 +0100

    Update ultraviner-refresh.user.js

--- ultraviner-refresh.user.js
+++ ultraviner-refresh.user.js
@@ -5,6 +5,7 @@
 // @description  Auto-refresh RFY
 // @author       Runew0lf
 // @match        https://www.amazon.co.uk/vine/vine-items?ultraviner=home*
+// @match        https://www.amazon.com/vine/vine-items?ultraviner=home*
 // @grant        none
 // ==/UserScript==
 
@@ -12,11 +13,15 @@
     'use strict';
 
     const seconds = 5;
+    const startHour = 9; //9am
+    const endHour = 17; //5pm
     const additionalRandomSeconds = 10; //can't be bigger than the seconds
 
     setInterval(() => {
         const randomUpToXSeconds = Math.random() * additionalRandomSeconds * 1000;
         const thisHour = new Date().getHours();
+
+//        if(thisHour > startHour && thisHour < endHour) {
 
             setTimeout(() => {
                 document.querySelector(`[data-queue-type="RFY"] [data-icon="arrows-rotate"]`).dispatchEvent(new MouseEvent('click', {
@@ -25,5 +30,6 @@
                     view: window
                 }));
             }, randomUpToXSeconds);
+//        }
     }, seconds * 1000);
 })();
